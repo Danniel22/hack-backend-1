90b8fdcd9f01decba2cd8f5ec4babe5391a5835c
Daniel22 <danielcorro222@gmail.com>
2023-12-15 19:19:17 -0400

    feat: add backend hacks

---
+++ h-6/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/h6', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def handle_request():
+    method = request.method
+    if method in ['GET', 'POST', 'DELETE']:
+        return jsonify({"method": method,
+                        "payload": "success",
+                        "error": False})
+    else:
+        return jsonify({})
+
+if __name__ == "__main__":
+    app.run(debug=True)
